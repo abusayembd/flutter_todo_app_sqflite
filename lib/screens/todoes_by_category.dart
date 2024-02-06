0c2e8c479efd07e34af30105ef576eb26d698fb8
unknown <abusayemsarker8@gmail.com>
2024-02-07 01:01:36 +0600

    todo list view in home screen, categories view in drawer

---
+++ lib/screens/todoes_by_category.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class TodosByCategory extends StatefulWidget {
+  const TodosByCategory({super.key});
+
+  @override
+  State<TodosByCategory> createState() => _TodosByCategoryState();
+}
+
+class _TodosByCategoryState extends State<TodosByCategory> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }গিত
+}
